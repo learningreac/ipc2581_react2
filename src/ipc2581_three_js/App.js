0dc9c89d337a13e073d3c05f47a196d80cc5ca71
learningreac <learningreact2020@gmail.com>
2022-04-07 02:32:59 -0700

    add line animation

--- src/ipc2581_three_js/App.js
+++ src/ipc2581_three_js/App.js
@@ -1,101 +1,110 @@
 import { useEffect, useRef } from 'react';
 import * as THREE from "three";
-import Stats from './js/stats.module';
+//import Stats from './js/stats.module';
+
+let theta = 0;
 
 const App = () => {
     const MountRef = useRef(null);
 
     useEffect(() => {
 
-       
-        let parentTransform;
-
+        let container, scene, camera, renderer, parentTransform;
         const pointer = new THREE.Vector2();
         const radius = 100;
-        let theta = 0;
+        
 
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        init();
+        animate();
 
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+/**
+ * define functions below
+ */
+        function init() {
+            container = document.createElement('div');
+            document.body.appendChild(container);
 
-        const lineGeometry = new THREE.BufferGeometry();
-        const points = [];
+            scene = new THREE.Scene();
+            scene.background = new THREE.Color(0xf0f0f0);
+            camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 
-        const point = new THREE.Vector3();
-        const direction = new THREE.Vector3();
+            renderer = new THREE.WebGLRenderer();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            renderer.setPixelRatio(window.devicePixelRatio);
+            container.appendChild(renderer.domElement);
 
-        for (let i = 0; i < 50; i++) {
+            const lineGeometry = new THREE.BufferGeometry();
+            const points = [];
 
-            direction.x += Math.random() - 0.5;
-            direction.y += Math.random() - 0.5;
-            direction.z += Math.random() - 0.5;
-            direction.normalize().multiplyScalar(10);
+            const point = new THREE.Vector3();
+            const direction = new THREE.Vector3();
 
-            point.add(direction);
-            points.push(point.x, point.y, point.z);
+            for (let i = 0; i < 50; i++) {
 
-        }
+                direction.x += Math.random() - 0.5;
+                direction.y += Math.random() - 0.5;
+                direction.z += Math.random() - 0.5;
+                direction.normalize().multiplyScalar(10);
 
-        lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(points, 3));
+                point.add(direction);
+                points.push(point.x, point.y, point.z);
+            }
 
-        parentTransform = new THREE.Object3D();
-        parentTransform.position.x = Math.random() * 40 - 20;
-        parentTransform.position.y = Math.random() * 40 - 20;
-        parentTransform.position.z = Math.random() * 40 - 20;
+            lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(points, 3));
 
-        parentTransform.rotation.x = Math.random() * 2 * Math.PI;
-        parentTransform.rotation.y = Math.random() * 2 * Math.PI;
-        parentTransform.rotation.z = Math.random() * 2 * Math.PI;
+            parentTransform = new THREE.Object3D();
+            parentTransform.position.x = Math.random() * 40 - 20;
+            parentTransform.position.y = Math.random() * 40 - 20;
+            parentTransform.position.z = Math.random() * 40 - 20;
 
-        parentTransform.scale.x = Math.random() + 0.5;
-        parentTransform.scale.y = Math.random() + 0.5;
-        parentTransform.scale.z = Math.random() + 0.5;
+            parentTransform.rotation.x = Math.random() * 2 * Math.PI;
+            parentTransform.rotation.y = Math.random() * 2 * Math.PI;
+            parentTransform.rotation.z = Math.random() * 2 * Math.PI;
 
-        for (let i = 0; i < 50; i++) {
+            parentTransform.scale.x = Math.random() + 0.5;
+            parentTransform.scale.y = Math.random() + 0.5;
+            parentTransform.scale.z = Math.random() + 0.5;
 
-            let object;
+            for (let i = 0; i < 50; i++) {
+                let object;
+                const lineMaterial = new THREE.LineBasicMaterial({ color: Math.random() * 0xffffff });
 
-            const lineMaterial = new THREE.LineBasicMaterial({ color: Math.random() * 0xffffff });
+                if (Math.random() > 0.5) {
+                    object = new THREE.Line(lineGeometry, lineMaterial);
+                } else {
+                    object = new THREE.LineSegments(lineGeometry, lineMaterial);
+                }
 
-            if (Math.random() > 0.5) {
+                object.position.x = Math.random() * 400 - 200;
+                object.position.y = Math.random() * 400 - 200;
+                object.position.z = Math.random() * 400 - 200;
 
-                object = new THREE.Line(lineGeometry, lineMaterial);
+                object.rotation.x = Math.random() * 2 * Math.PI;
+                object.rotation.y = Math.random() * 2 * Math.PI;
+                object.rotation.z = Math.random() * 2 * Math.PI;
 
-            } else {
+                object.scale.x = Math.random() + 0.5;
+                object.scale.y = Math.random() + 0.5;
+                object.scale.z = Math.random() + 0.5;
 
-                object = new THREE.LineSegments(lineGeometry, lineMaterial);
+                parentTransform.add(object);
 
             }
 
-            object.position.x = Math.random() * 400 - 200;
-            object.position.y = Math.random() * 400 - 200;
-            object.position.z = Math.random() * 400 - 200;
-
-            object.rotation.x = Math.random() * 2 * Math.PI;
-            object.rotation.y = Math.random() * 2 * Math.PI;
-            object.rotation.z = Math.random() * 2 * Math.PI;
-
-            object.scale.x = Math.random() + 0.5;
-            object.scale.y = Math.random() + 0.5;
-            object.scale.z = Math.random() + 0.5;
-
-            parentTransform.add(object);
-
-        }
-
-        scene.add(parentTransform);
-
-        camera.position.z = 5;
+            scene.add(parentTransform);
+            // camera.position.z = 5;
+        };
 
         function animate() {
             requestAnimationFrame(animate);
+            render();
 
+        };
+
+        function render() {
             theta += 0.1;
 
-            if (0) {
+            if (1) {
                 camera.position.x = radius * Math.sin(THREE.MathUtils.degToRad(theta));
                 camera.position.y = radius * Math.sin(THREE.MathUtils.degToRad(theta));
                 camera.position.z = radius * Math.cos(THREE.MathUtils.degToRad(theta));
@@ -104,9 +113,9 @@ const App = () => {
             camera.updateMatrixWorld();
 
             renderer.render(scene, camera);
-        };
+        }
 
-        animate();
+
 
     }, []);
 
